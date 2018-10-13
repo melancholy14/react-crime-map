7ad2f4f594e651b8c56555f22c77bd26d0f63273
Misol <misol.goh@gmail.com>
2018-10-13 21:03:19 +0100

    Updates

---
+++ precache-manifest.7e8070492c6bdec25e786e307bc223d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec97d754becc2140104b",
+    "url": "/react-crime-map/static/js/runtime~main.ec97d754.js"
+  },
+  {
+    "revision": "cfcd293fbf9322b3c5ea",
+    "url": "/react-crime-map/static/js/main.cfcd293f.chunk.js"
+  },
+  {
+    "revision": "bede3b53788150034025",
+    "url": "/react-crime-map/static/js/1.bede3b53.chunk.js"
+  },
+  {
+    "revision": "cfcd293fbf9322b3c5ea",
+    "url": "/react-crime-map/static/css/main.aadc3d67.chunk.css"
+  },
+  {
+    "revision": "adbad3b8f068363143a433d20d3c8364",
+    "url": "/react-crime-map/index.html"
+  }
+];
