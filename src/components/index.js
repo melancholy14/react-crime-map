2225421906ce5e92f499051286dcdcea93658a3e
Misol <melancholy8914@gmail.com>
2018-10-18 08:01:46 +0100

    Adds loading component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Modal from './modal';
 import { Table, Tr, Th, Td } from './table';
+import Loading from './loading';
 
 export {
   Modal,
   Table, Tr, Th, Td,
+  Loading,
 };
---
+++ src/components/loading.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Modal from './modal';
+
+import iu from '../assets/iu.gif'
+import boa from '../assets/boa.gif';
+
+const LoadingDiv = styled.div` 
+  img {
+    width: 100%;
+  }
+`;
+
+const Loading = ({ loading }) => (
+  <Modal
+    title="Loading..."
+    show={loading}
+    showFooter={false}
+  >
+    <LoadingDiv>
+      <img src={Math.round(Math.random()) % 2 === 0 ? boa : iu} alt="loading..." />
+    </LoadingDiv>
+  </Modal>
+);
+
+Loading.propTypes = {
+  loading: PropTypes.bool,
+}
+
+export default Loading;
