a62af20c0bdc5b4d086fa2cfbc74c334461464c2
Misol <melancholy8914@gmail.com>
2019-02-03 14:39:42 +0000

    Update: add styled table in Neighbourhood

--- src/containers/AnalysePage/neighbourbood.jsx
+++ src/containers/AnalysePage/neighbourbood.jsx
@@ -1,6 +1,30 @@
 // @flow
 
 import React from 'react';
+import Parser from 'html-react-parser';
+import styled from 'styled-components';
+
+import {
+  Table, Tr, Th, Td,
+} from '../../components';
+
+const Div = styled.div`
+  padding: 1rem;
+  font-size: smaller;
+`;
+
+const Italic = styled.div`
+  font-style: italic;
+  font-weight: lighter;
+  font-size: larger;
+`;
+
+const Team = styled.div`
+  border: 1px solid #a4a4a4;
+  border-radius: 5px;
+  padding: 0.5rem;
+  margin: 0.5rem 0;
+`;
 
 const Neighbourbood = React.memo(({ data = {} }: { data: Object }) => {
   const {
@@ -29,50 +53,69 @@ const Neighbourbood = React.memo(({ data = {} }: { data: Object }) => {
   console.log(priorities);
 
   return (
-    <div>
-      <div>{name}</div>
-      <div>{force}</div>
+    <Div>
+      <h3>{name}</h3>
       <ul>
+        { force && <li>{force}</li> }
         { email && <li>{email}</li> }
         { facebook && <li>{facebook}</li> }
         { twitter && <li>{twitter}</li> }
       </ul>
-      <div>{description}</div>
+      <div>{description && Parser(description)}</div>
+      <hr />
+      <h4>You may want to know...</h4>
       <div>
         { links && links.map(({ title, url }, index) => (
           <div key={title}>
-            <div>{title}</div>
+            <Italic>{title}</Italic>
             <div>{url}</div>
-            <div>{locations[index].address}</div>
-            <div>{locations[index].postcode}</div>
-            <div>{locations[index].name}</div>
+            <div>
+              {`${locations[index].name}, ${locations[index].address}, ${locations[index].postcode}`}
+            </div>
           </div>
         ))}
       </div>
       <hr />
+      <h4>TEAM</h4>
       <div>
         {
           team && team.map(({ name: tname, bio }) => (
-            <div key={tname}>
-              <div>{ tname }</div>
-              <div>{ bio }</div>
-            </div>
+            <Team key={tname}>
+              <Italic>{tname}</Italic>
+              <div>{bio && Parser(bio)}</div>
+            </Team>
           ))
         }
       </div>
       <hr />
+      <h4>EVENTS</h4>
       <div>
         {
           events && events.map(({
-            title, start_date: start, end_date: end, type, address,
+            title, start_date: start, end_date: end, type, address, description: desc,
           }) => (
-            <div key={`${title}_${start}_${end}`}>
-              <div>{ title }</div>
-              <div>{ start }</div>
-              <div>{ end }</div>
-              <div>{ type }</div>
-              <div>{ address }</div>
-            </div>
+            <Table key={`${title}_${start}_${end}`}>
+              <Tr>
+                <Th>Title</Th>
+                <Td>{title}</Td>
+              </Tr>
+              <Tr>
+                <Th>Type</Th>
+                <Td>{type}</Td>
+              </Tr>
+              <Tr>
+                <Th>Date</Th>
+                <Td>{`${start} ~ ${end}`}</Td>
+              </Tr>
+              <Tr>
+                <Th>Address</Th>
+                <Td>{address}</Td>
+              </Tr>
+              <Tr>
+                <Th>Description</Th>
+                <Td>{desc && Parser(desc)}</Td>
+              </Tr>
+            </Table>
           ))
         }
       </div>
@@ -80,16 +123,24 @@ const Neighbourbood = React.memo(({ data = {} }: { data: Object }) => {
       <div>
         {
           priorities && priorities.map(({ issue, action, ...dates }) => (
-            <div key={`${issue}_${action}`}>
-              <div>{ issue }</div>
-              <div>{ dates['issue-date'] }</div>
-              <div>{ action }</div>
-              <div>{ dates['action-date'] }</div>
-            </div>
+            <Table key={`${issue}_${action}`}>
+              <Tr>
+                <Th>Issue</Th>
+                <Td>{dates['issue-date']}</Td>
+                <Td>{issue && Parser(issue)}</Td>
+              </Tr>
+              { action && (
+              <Tr>
+                <Th>action</Th>
+                <Td>{dates['action-date']}</Td>
+                <Td>{action && Parser(action)}</Td>
+              </Tr>
+              )}
+            </Table>
           ))
         }
       </div>
-    </div>
+    </Div>
   );
 });
 
