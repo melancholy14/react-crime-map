a018bfe7f2a5cd923274d3fb4b72aea3aae161f3
Misol <melancholy8914@gmail.com>
2019-02-05 09:07:53 +0000

    Updates

---
+++ precache-manifest.d9c314fa02b01518e99352d94798d858.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec97d754becc2140104b",
+    "url": "/react-crime-map/static/js/runtime~main.ec97d754.js"
+  },
+  {
+    "revision": "0cbd5e78af96de56c2ff",
+    "url": "/react-crime-map/static/js/main.0cbd5e78.chunk.js"
+  },
+  {
+    "revision": "c35aea91780614133eec",
+    "url": "/react-crime-map/static/js/1.c35aea91.chunk.js"
+  },
+  {
+    "revision": "0cbd5e78af96de56c2ff",
+    "url": "/react-crime-map/static/css/main.3019ac3d.chunk.css"
+  },
+  {
+    "revision": "0d6f787cec208dc41aacb26b924db7ce",
+    "url": "/react-crime-map/index.html"
+  }
+];
