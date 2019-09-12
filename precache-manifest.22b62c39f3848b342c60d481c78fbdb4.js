9a65611f08862a030297be0e787a15f73effe6bc
melancholy14 <melancholy8914@gmail.com>
2019-09-12 02:07:44 +0100

    Updates

---
+++ precache-manifest.22b62c39f3848b342c60d481c78fbdb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a0bf457b8dbe931a6a0",
+    "url": "/react-crime-map/static/css/main.4a96cbec.chunk.css"
+  },
+  {
+    "revision": "8a0bf457b8dbe931a6a0",
+    "url": "/react-crime-map/static/js/main.8a0bf457.chunk.js"
+  },
+  {
+    "revision": "fb7c99623be2870613f4",
+    "url": "/react-crime-map/static/js/1.fb7c9962.chunk.js"
+  },
+  {
+    "revision": "ec97d754becc2140104b",
+    "url": "/react-crime-map/static/js/runtime~main.ec97d754.js"
+  },
+  {
+    "revision": "b633cfa4c1067edd2cafaf91c85268de",
+    "url": "/react-crime-map/index.html"
+  }
+];
