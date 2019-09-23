4dd98b72819b635c436b4c800a987ff76a892415
melancholy14 <melancholy8914@gmail.com>
2019-09-23 03:19:52 +0100

    Update header.jsx
    able to toggle div which includes email

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,7 +10,7 @@ const HeaderDiv = styled.header`
     background: darkslateblue;
     color: yellow;
     line-height: 2rem;
-    height: 2.5rem;
+    // height: 2.5rem;
     vertical-align: middle;
     padding: 0;
     margin: 0;
@@ -19,8 +19,12 @@ const HeaderDiv = styled.header`
     display: flex;
 
     @media screen and (min-width: 768px) {
-      height: 4rem;
+      // height: 4rem;
       display: initial;
+    }
+
+    svg {
+      margin: auto 0.5rem;
     }
 `;
 
@@ -35,11 +39,17 @@ const Title = styled.div`
   }
 `;
 
+const InlineDiv = styled.div`
+  display: inline;
+`;
+
 const Email = styled.div`
   width: 10rem;
   text-align: right;
   padding: 0 1rem 0 0;
   font-size: small;
+
+  display: ${props => (props.show ? 'block' : 'none')};
 
   @media screen and (min-width: 768px) {
     width: initial;
@@ -52,17 +62,42 @@ const Email = styled.div`
   }
 `;
 
-const Header = () => (
-  <HeaderDiv>
-    <Title>
-      CRIME MAP
-      <FontAwesomeIcon icon={faExclamationCircle} />
-    </Title>
-    <Email>
-      <span className="smaller">
-        <a href="mailto:melancholy8914@gmail.com">melancholy8914@gmail.com</a>
-      </span>
-    </Email>
-  </HeaderDiv>);
+type Props = {};
 
-export default Header;
+type State = {
+  toggle: boolean,
+};
+
+export default class Header extends React.PureComponent<Props, State> {
+  constructor() {
+    super();
+
+    this.state = {
+      toggle: false,
+    };
+  }
+
+  toggle = () => {
+    const { toggle } = this.state;
+    this.setState({ toggle: !toggle });
+  }
+
+  render() {
+    const { toggle } = this.state;
+
+    return (
+      <HeaderDiv>
+        <Title>
+          CRIME MAP
+          <InlineDiv onClick={this.toggle} role="button" tabIndex={0} onKeyPress={this.toggle}>
+            <FontAwesomeIcon icon={faExclamationCircle} size="sm" />
+          </InlineDiv>
+        </Title>
+        <Email show={toggle}>
+          <span className="smaller">
+            <a href="mailto:melancholy8914@gmail.com">melancholy8914@gmail.com</a>
+          </span>
+        </Email>
+      </HeaderDiv>);
+  }
+}
